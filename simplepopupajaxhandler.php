08af0672493956c804dfed755346118138b5786a
Shlomo Meyer <shlomomeyer21@gmail.com>
2020-05-17 13:01:55 +0300

    missing php tag

--- simplepopupajaxhandler.php
+++ simplepopupajaxhandler.php
@@ -1,3 +1,4 @@
+<?php
 abstract class AJAXHandler
 {
 public function handleRequest()
@@ -36,4 +37,3 @@ echo "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod te
 
 $handler = new SimplePopupAJAXHandler();
 $handler->handleRequest();
-
